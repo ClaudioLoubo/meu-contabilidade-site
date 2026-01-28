5c21139d58294963815732e1aa162258dfa5cfdb
ClaudioLoubo <claudioloubo@gmail.com>
2026-01-28 11:05:47 -0300

    configurando gitignore e fixando deploy setup

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/meu-contabilidade-site/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src/imgs'),
